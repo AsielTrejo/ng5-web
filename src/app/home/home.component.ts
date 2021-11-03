f4d6d176f63fbe3a15df009c674da980341eb04e
AsielTrejo <asieltrejo@gmail.com>
2021-11-03 07:34:30 -0600

    Solves #11

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
 //Animation Done
 
 import { Component, OnInit } from '@angular/core';
-import{ trigger,style, transition, animate, keyframes, query, stagger } from '@angular/animations'
+import { trigger,style, transition, animate, keyframes, query, stagger } from '@angular/animations';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -41,22 +42,26 @@ export class HomeComponent implements OnInit {
   itemCount: number=0;
   btnText: string = 'Add an item';
   goalText: string = 'My first life goal';
-  goals:string[]=['My first life goal','I want to climb a mountain', 'Go ice Skiing'];
+  goals:string[]=[];
 
-  constructor() { }
+  constructor(private _data: DataService) { }
 
   ngOnInit(){
+    this._data.goal.subscribe(res => this.goals = res);
     this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
   }
 
   addItem(){
     this.goals.push(this.goalText);
     this.goalText='';
     this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
   }
 
   removeItem( i:number) {
     this.goals.splice(i,1);
+    this._data.changeGoal(this.goals);
   }
 
 }
