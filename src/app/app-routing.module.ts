927f132365e78a2a9afa9336dd25f8608ade45cc
AsielTrejo <asieltrejo@gmail.com>
2021-11-03 03:31:30 -0600

    errased final routing #7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-
-const routes: Routes = [
-{
-  path: '',
-  component: HomeComponent
-},
-{
-  path: 'about',
-  component: AboutComponent
-}
 
 
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
